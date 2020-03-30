9d26edf2cecef7011282ac444ca69e1c0615384d
Sviatoslav Dackovskij <sviatoslav_dackovskij@epam.com>
2020-03-30 15:57:26 +0300

    add directive and fixed problems

--- src/app/pokemon/main-page/main-page.component.ts
+++ src/app/pokemon/main-page/main-page.component.ts
@@ -9,8 +9,6 @@ export class MainPageComponent implements OnInit {
 
 
   toggle = true;
-  
- 
 
   constructor() { }
 
--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -3,18 +3,20 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { CardComponent } from './card/card.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { StyleDirective } from './directives/style.directive';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, CardComponent, MainPageComponent],
+  declarations: [HeaderComponent, CardComponent, MainPageComponent, StyleDirective],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
     CardComponent,
-    MainPageComponent
+    MainPageComponent,
+    StyleDirective
   ]
 })
 export class PokemonModule { }
